8ee62e46b4e3ad15e66c8275cfdc9daecb83c7d7
Ed Cliffe <ed.cliffe1@gmail.com>
2022-05-12 21:17:59 +0100

    batch_processing

--- API/batch_consumer.py
+++ API/batch_consumer.py
@@ -1,4 +1,8 @@
 from kafka import KafkaConsumer
+import boto3 
+from datetime import datetime
+import uuid
+import json
 from json import loads
 
 # create our consumer to retrieve the message from the topics
@@ -10,5 +14,13 @@ data_batch_consumer = KafkaConsumer(
 
 data_batch_consumer.subscribe(topics=["PinterestTopic"])
 
+s3_client = boto3.client('s3')
+
 for msg in data_batch_consumer:
-    print("PinterestTopic=%s,Message=%s"%(msg.topic,msg.value))
+    message_id = uuid.uuid4
+    with open("message.json", "w") as outfile:
+        json.dump("PinterestTopic=%s,Message=%s"%(msg.topic,msg.value), outfile)
+    
+    response = s3_client.upload_file("message.json" , 'pinbucket2', f"{message_id}")
+
+# Need to sort out aws access keys and so on...
