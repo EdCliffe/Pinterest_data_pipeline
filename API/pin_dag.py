86900c3fbb4286e4bdcfbbe4acf5fcffcf2422e4
Ed Cliffe <ed.cliffe1@gmail.com>
2022-07-06 22:01:06 +0100

    airflow

---
+++ API/pin_dag.py
@@ -0,0 +1,37 @@
+from airflow.models import DAG
+from datetime import datetime
+from datetime import timedelta
+from airflow.operators.bash_operator import BashOperator
+
+
+default_args = {
+    'owner': 'Ivan',
+    'depends_on_past': False,
+    'email': ['ivan@theaicore.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'start_date': datetime(2020, 1, 1),
+    'retry_delay': timedelta(minutes=5),
+    'end_date': datetime(2022, 1, 1),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'trigger_rule': 'all_success'
+}
+
+with DAG(dag_id='pin_dag',
+         default_args=default_args,
+         schedule_interval='*/1 * * * *',
+         catchup=False,
+         tags=['test']
+         ) as dag:
+    # Define the tasks. Here we are going to define only one bash operator
+
+    # Spark-submit - Run S3 to spark connector, stores in cassandra
+    spark_batch = BashOperator(
+        task_id='s3_to_spark_connector',
+        bash_command="spark-submit --packages com.amazonaws:aws-java-sdk-s3:1.12.196,org.apache.hadoop:hadoop-aws:3.3.1,com.datastax.spark:spark-cassandra-connector_2.12:3.2.0 ~/Work/Packages/Pinterest_data_pipeline/API/s3_to_spark_connector.py"
+        dag=dag)
