f954f3c692ae573af976b8731d1235d249d9ef8e
Ed Cliffe <ed.cliffe1@gmail.com>
2022-07-12 14:03:44 +0100

    stream notes

--- API/kafka_to_spark_stream.py
+++ API/kafka_to_spark_stream.py
@@ -40,4 +40,21 @@ stream_df.writeStream \
     .awaitTermination()
 
     # Process data, extract 2 metrics, clean a bit, save to disk? save to s3? add a checkpoint?
-    
+
+    # time window, posts per minute received
+
+# Running count function
+def updateFunction(newValues, runningCount):
+    if runningCount is None:
+        runningCount = 0
+    return sum(newValues, runningCount)
+
+stream_df.updateStateByKey(updateFunction) #allows us to keep one state and update it continuously with values coming from a stream
+
+
+    # post per minute errors, and % total nulls to date
+    # saving the ID of the error, or message title
+
+    # save a list of null posts, save to file
+
+# saveAsTextFiles(prefix, [suffix]) 	Save this DStream's contents as text files. The file name at each batch interval is generated based on prefix and suffix: "prefix-TIME_IN_MS[.suffix]".
